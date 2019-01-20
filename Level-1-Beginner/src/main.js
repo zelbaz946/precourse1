2c9c9d0a9191d8e81d6b481cadfbc9312b0be924
Shane Taylor <shaneallantaylor@gmail.com>
2019-01-19 17:03:08 -0800

    all level 1 passing

--- Level-1-Beginner/src/main.js
+++ Level-1-Beginner/src/main.js
@@ -2,16 +2,14 @@
 // ex: addTwo(5); -> 7
 // ex: addTwo(6); -> 8
 function addTwo(number) {
-	//CODE HERE
-
+	return number + 2;
 }
 
 
 // adds the two inputted numbers together
 // ex: add(5, 10); -> 15
 function add(num1, num2) {
-	//CODE HERE
-
+	return num1 + num2;
 }
 
 
@@ -19,16 +17,14 @@ function add(num1, num2) {
 // ex: addd(1,2,3); -> 6
 // ex: addd(2,4,2); -> 8
 function addd(value1, value2, value3) {
-	//CODE HERE
-
+	return value1 + value2 + value3;
 }
 
 
 // subtracts the second inputted number from the first inputted number
 // ex: subtract(5, 10); -> -5
 function subtract(n1, n2) {
-	//CODE HERE
-
+	return n1 - n2;
 }
 
 
@@ -36,8 +32,7 @@ function subtract(n1, n2) {
 // ex: multiplyByTwo(5); -> 10
 // ex: multiplyByTwo(6); -> 12
 function multiplyByTwo(numero) {
-	//CODE HERE
-
+	return numero * 2;
 }
 
 
@@ -45,8 +40,7 @@ function multiplyByTwo(numero) {
 // ex: multiply(1,2); -> 2
 // ex: multiply(2,2); -> 4
 function multiply(val1, val2) {
-	//CODE HERE
-
+ return val1 * val2;
 }
 
 
@@ -54,16 +48,14 @@ function multiply(val1, val2) {
 // ex: multiplyy(1,2,3); -> 6
 // ex: multiplyy(2,2,3); -> 12
 function multiplyy(a, b, c) {
-	//CODE HERE
-
+	return a * b * c;
 }
 
 
 // returns the square of the inputted number
 // ex: square(5); -> 25
 function square(value) {
-	//CODE HERE
-
+	return value * value;
 }
 
 
@@ -73,8 +65,10 @@ function square(value) {
 // ex: isOdd(4); -> false
 // ex: isOdd(5); -> true
 function isOdd(num) {
-	//CODE HERE
-
+	if (num % 2 === 1) {
+		return true;
+	}
+	return false;
 }
 
 
@@ -85,8 +79,10 @@ function isOdd(num) {
 // ex: isNegative(1); -> false
 // ex: isNegative(0); -> false
 function isNegative(num) {
-	//CODE HERE
-
+	if (num >= 0) {
+		return false;
+	}
+	return true;
 }
 
 
@@ -94,31 +90,31 @@ function isNegative(num) {
 // ex: positive(-1); -> 1
 // ex: positive(1); -> 1
 function positive(num) {
-	//CODE HERE
-
+	if (isNegative(num) === true) {
+		return num * -1;
+	}
+	return num;
 }
 
 // Returns boolean of whether argument is classified as a Number object
 // isNumber(5); → true
 // isNumber('hi'); → false
 function isNumber(value) {
-	//CODE HERE
-
+	return Number.isInteger(value);
 }
 
 // Returns boolean of whether argument is classified as an Array object
 // isArray(5); → false
 // isArray([1,2,3]); → true
 function isArray(value) {
-	//CODE HERE
-
+	return Array.isArray(value);
 }
 
 // Returns boolean of whether argument is classified as an Object
 // isObject(5); → false
 // isObject([1,2,3]); → true
 function isObject(value) {
-	//CODE HERE
+	return (typeof value === 'object' && value !== null);
 
 }
 
@@ -126,7 +122,6 @@ function isObject(value) {
 // isNull(null); -> true
 // isNull(5); -> false
 function isNull(value) {
-	//CODE HERE
-
+	return (value === null);
 }
 
