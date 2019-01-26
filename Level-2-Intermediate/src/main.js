b412de5c943aa70abb9e65a7c181b41a66fd239d
Shane Taylor <shaneallantaylor@gmail.com>
2019-01-26 14:53:45 -0800

    all but final test

--- Level-2-Intermediate/src/main.js
+++ Level-2-Intermediate/src/main.js
@@ -2,8 +2,8 @@
 // and returns an array of only the odd numbers
 // ex: returnOdds([1,2,3,4,5,6,7]); -> [1,3,5,7]
 function returnOdds(array) {
-	//CODE HERE
-
+// lets use filter!
+	return array.filter(value => value % 2 === 1);
 }
 
 
@@ -11,15 +11,20 @@ function returnOdds(array) {
 // and returns an array of only the even numbers
 // ex: returnEvent([1,2,3,4,5,6,7]); -> [2,4,6]
 function returnEvens(array) {
-	//CODE HERE
-
+	return array.filter(value => value % 2 === 0);
 }
 
 
 // returns only the max element from the inputted array of numbers
 // ex: findMax([1,25,6,3]); -> 25
 function findMax(array) {
-	//CODE HERE
+// lets use reduce!
+	return array.reduce(function(acc, val, index, array) {
+		if (val > acc) {
+			acc = val;
+		}
+		return acc;
+	});
 
 }
 
@@ -28,6 +33,7 @@ function findMax(array) {
  * trim(' hello '); -> 'hello'
  */
 function trim(string) {
+	return string.trim();
 
 }
 
@@ -38,6 +44,20 @@ function trim(string) {
 // shift() removes a value from the beginning and returns it
 // the goal of this problem is to reverse engineer what array methods are actually doing and return an object that has those methods
 function createArray() {
-	//CODE HERE
+  this.arr = [];
+  this.index = 0;
+};
 
+createArray.prototype.push = function( value ) {
+  this.arr[ this.index++ ] = value;
+  return this;
+};
+
+// lets alter this array
+// lets make a new array with all the values from the previous array
+// and stop just short of the final item
+
+createArray.prototype.pop = function( value ) {
+  this.arr[this.arr.length] = null;
+	return this;
 }
