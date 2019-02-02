73024e8135d403de3ac629e580cf411802f2e462
Shane Taylor <shaneallantaylor@gmail.com>
2019-02-02 11:40:33 -0800

    all passing

--- Level-2-Intermediate/src/main.js
+++ Level-2-Intermediate/src/main.js
@@ -2,7 +2,7 @@
 // and returns an array of only the odd numbers
 // ex: returnOdds([1,2,3,4,5,6,7]); -> [1,3,5,7]
 function returnOdds(array) {
-// lets use filter!
+	// lets use filter!
 	return array.filter(value => value % 2 === 1);
 }
 
@@ -18,8 +18,8 @@ function returnEvens(array) {
 // returns only the max element from the inputted array of numbers
 // ex: findMax([1,25,6,3]); -> 25
 function findMax(array) {
-// lets use reduce!
-	return array.reduce(function(acc, val, index, array) {
+	// lets use reduce!
+	return array.reduce(function (acc, val, index, array) {
 		if (val > acc) {
 			acc = val;
 		}
@@ -44,20 +44,52 @@ function trim(string) {
 // shift() removes a value from the beginning and returns it
 // the goal of this problem is to reverse engineer what array methods are actually doing and return an object that has those methods
 function createArray() {
-  this.arr = [];
-  this.index = 0;
+	let array = Object.create(createArray.prototype);
+	array.length = 0;
+	return array;
 };
 
-createArray.prototype.push = function( value ) {
-  this.arr[ this.index++ ] = value;
-  return this;
-};
-
-// lets alter this array
-// lets make a new array with all the values from the previous array
-// and stop just short of the final item
-
-createArray.prototype.pop = function( value ) {
-  this.arr[this.arr.length] = null;
-	return this;
+createArray.prototype.push = function (val) {
+	let currentLength = JSON.stringify(this.length);
+	this[currentLength] = val;
+	this.length += 1;
 }
+
+createArray.prototype.pop = function () {
+	this.length -= 1;
+	let currentLength = JSON.stringify(this.length);
+	let output = this[currentLength];
+	delete this[currentLength];
+
+	return output;
+}
+
+createArray.prototype.unshift = function (val) {
+	this.length += 1;
+	let currentValue = null;
+	for (let i = this.length - 1; i > 0; i -= 1) {
+		currentValue = this[JSON.stringify(i - 1)];
+		this[JSON.stringify(i)] = currentValue;
+	}
+	this["0"] = val;
+}
+
+createArray.prototype.shift = function () {
+	// remove first thing from array
+	// return that thing
+	let keyToRemove = this.length - 1;
+	let output = this["0"];
+	let currentValue = null;
+	// for loop to move all values 'down' a key
+	for (let i = 0; i < this.length - 1; i += 1) {
+		currentValue = this[JSON.stringify(i + 1)];
+		this[JSON.stringify(i)] = currentValue;
+	}
+
+	delete this[keyToRemove];
+	this.length -= 1;
+	return output;
+}
+
+
+const shane = createArray();
